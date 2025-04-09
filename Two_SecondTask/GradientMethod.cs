6d659335eb923d501d1d0df36e1a85d7a561755f
oduvanchikm <mkgubareva2005@gmail.com>
2025-04-09 18:42:35 +0400

    second in second task is done

---
+++ Two_SecondTask/GradientMethod.cs
@@ -0,0 +1,92 @@
+namespace Two_SecondTask;
+
+public class GradientMethod
+{
+    public static void GradientMethodSolution(double[][] points, double[] initialGuess)
+    {
+        double eps = 1e-6; 
+        int iteration = 0;
+        double[] x = initialGuess; 
+        int n = points[0].Length;
+        double alpha = 0.1;
+
+        while (true)
+        {
+            double[] g = CalculateGradient(points, x, n);
+
+            if (Norm(g) < eps)
+                break;
+
+            x = UpdateSolution(x, g, alpha, n);
+
+            iteration++;
+            Console.WriteLine($"Iteration {iteration}: Norm(g) = {Norm(g):E6}");
+        }
+
+        Console.WriteLine("\nFinal solution:");
+        for (int i = 0; i < x.Length; i++)
+        {
+            Console.WriteLine($"x[{i}] = {x[i]:F6}");
+        }
+    }
+
+    private static double[] CalculateGradient(double[][] points, double[] x, int n)
+    {
+        double[] g = new double[n];
+        for (int i = 0; i < n; i++)
+        {
+            g[i] = 0;
+        }
+
+        foreach (var point in points)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                g[i] += (x[i] - point[i]);
+            }
+        }
+
+        return g;
+    }
+
+    private static double[] UpdateSolution(double[] x, double[] g, double alpha, int n)
+    {
+        double[] result = new double[n];
+        for (int i = 0; i < n; i++)
+        {
+            result[i] = x[i] - alpha * g[i]; 
+        }
+
+        return result;
+    }
+
+    public static double[] ExactSolution(double[][] points)
+    {
+        int n = points[0].Length; 
+        double[] result = new double[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            result[i] = 0;
+            foreach (var point in points)
+            {
+                result[i] += point[i];
+            }
+
+            result[i] /= points.Length;
+        }
+
+        return result;
+    }
+
+    private static double Norm(double[] b)
+    {
+        double sum = 0;
+        for (int i = 0; i < b.Length; ++i)
+        {
+            sum += b[i] * b[i];
+        }
+
+        return Math.Sqrt(sum);
+    }
+}
---
+++ Two_SecondTask/Program.cs
@@ -0,0 +1,18 @@
+﻿using Two_SecondTask;
+
+double[][] points = new double[][]
+{
+    new double[] { 1, 2 },
+    new double[] { 3, 4 },
+    new double[] { 5, 6 },
+    new double[] { 7, 8 }
+};
+
+double[] initialGuess = new double[] { 0, 0 };
+
+Console.WriteLine("Solution with Gradient Method:");
+GradientMethod.GradientMethodSolution(points, initialGuess);
+
+double[] exactSolution = GradientMethod.ExactSolution(points);
+Console.WriteLine("Solution:");
+Console.WriteLine($"x = ({exactSolution[0]:F6}, {exactSolution[1]:F6})");
